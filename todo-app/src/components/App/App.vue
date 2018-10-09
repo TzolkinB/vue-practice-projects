c91934a0f7a303866e8f0d69781d21f2a1da9626
TzolkinB <krbell4@gmail.com>
2018-10-08 21:58:03 -0400

    course complete

--- todo-app/src/components/App/App.vue
+++ todo-app/src/components/App/App.vue
@@ -1,13 +1,19 @@
 <template>
 	<div class="container-fluid mt-3">
 	  <p>{{ header }}</p>
-	  <input type="text" v-model="newItem" v-on:keyup.enter="addItem(newItem)"placeholder="Enter a todo item here" />
-	  <button v-on:click="addItem(newItem)">Add</button>
-	  <!-- v-on can be shortened to @ -->
-	  <!-- example: @click="onAdd(newItem)" -->
+	  <button v-if="state === 'default'" class="btn btn-raised btn-danger" 
+	  v-on:click="changeState('edit')">Add Item</button>
+	  <span v-else></span>
+	  <div v-if="state === 'edit'">
+	  	<input type="text" v-model="newItem" v-on:keyup.enter="addItem(newItem)"placeholder="Enter an item here" />
+	  	<button v-on:click="addItem(newItem)" v-bind:disabled="newItem.length === 0" class="btn btn-raised btn-success">Add Item</button>
+	  	<!-- v-on can be shortened to @ -->
+	  	<!-- example: @click="onAdd(newItem)" -->
+	  </div>
     <ul>
-      <li v-for="todo in todos">{{ todo }}</li>
+      <li v-for="item in items">{{ item }}</li>
     </ul>
+    <p v-if="items.length === 0">What? Nothing to do!?</p>
 	</div>
 </template>
 
@@ -19,23 +25,25 @@
 	      if (newItem && newItem !== '') {
 	        console.log('input', newItem);
 	        return(
-	        	this.todos = [ ...this.todos, newItem],
-	        	//return (this.todos.push(newItem));
+	        	this.items = [ ...this.items, newItem],
+	        	//return (this.items.push(newItem));
 	        	this.newItem = '' // clears input on enter
 	        );
 	      }
 	      return console.log('oops no new item');
-	    }
+	    },
+	    changeState(newState) {
+	    	this.state = newState;
+	    	this.newItem = '';
+	    },
 	  },
 	  data() {
 	  	return {
+	  		state: 'default',
 		  	header: "Vue To Do List",
 		  	newItem: '',
-		    todos: [
-		      "Thing 1",
-		      "Thing 2",
-		      "Thing 3",
-		      "Thing 4",
+		    items: [
+		      
 		    ],
 	  	};
 	  }
